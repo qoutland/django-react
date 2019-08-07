a544a367847b49a398e04a2d76f1ebb7ee7a5035
qoutland <qoutland@gmail.com>
2019-08-07 10:24:17 -0700

    Finished Tut

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,68 @@
 import React, { Component } from 'react';
 import './App.css';
-
-const todoItems = [
-  {
-    id: 1,
-    description: "Task 1",
-    completed: true
-  },
-  {
-    id: 2,
-    description: 'Task 2',
-    completed: false
-  }
-]
+import Modal from './components/Modal';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal: false,
       viewCompleted: false,
-      todoList: todoItems
+      todoList: [],
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      }
     };
+  }
+
+  componentDidMount = () => {
+    this.refreshList()
+  }
+
+  refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/todos")
+      .then(res => this.setState({ todoList: res.data }))
+      .catch(err => console.log(err));
+    }
+
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal } )
+  }
+
+  handleSubmit = item => {
+    this.toggle();
+    console.log(item.id)
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+        .then(res => this.refreshList());
+        alert('POST');
+      return
+    }
+    axios
+    .post('http://localhost:8000/api/todos/', item)
+    .then(res => this.refreshList());
+    alert('POST');
+  }
+
+  handleDelete = item => {
+    axios
+      .delete(`http://localhost:8000/api/todos/${item.id}/`)
+      .then(res => this.refreshList())
+  }
+
+  createItem = () => {
+    const item = { title: "", description: "", completed: false};
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  }
+
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal })
   }
 
   displayCompleted = status => {
@@ -48,7 +90,7 @@ class App extends Component {
   };
 
   renderItems = () => {
-    const { viewCompleted } = this.state.viewCompleted;
+    const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(item => item.completed === viewCompleted);
     return newItems.map(item => (
       <li 
@@ -60,9 +102,14 @@ class App extends Component {
         title={item.description}
       > {item.title}
       </span>
+      <span
+        className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}
+        title={item.description}
+      > {item.description}
+      </span>
       <span>
-        <button className="btn btn-secondary mr-2">Edit</button>
-        <button className="btn btn-danger">Delete</button>
+        <button className="btn btn-secondary mr-2" onClick={ () => this.editItem(item)}>Edit</button>
+        <button className="btn btn-danger" onClick={ () => this.handleDelete(item) }>Delete</button>
       </span>
       </li>
     ));
@@ -76,7 +123,7 @@ class App extends Component {
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
               <div className="">
-                <button className="btn btn-primary">Add Task</button>
+                <button className="btn btn-primary" onClick={this.createItem}>Add Task</button>
               </div>
               {this.renderTabList()}
               <ul className="list-group list-group-flush">
@@ -85,6 +132,13 @@ class App extends Component {
             </div>
           </div>
         </div>
+        { this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null }
       </main>
     );
   }
